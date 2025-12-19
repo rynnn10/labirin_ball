fb80e851b8720e186c1080c64b6cc48e0e4218ee
RIYAN <137081303+rynnn10@users.noreply.github.com>
2025-12-19 22:29:55 +0700

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,59 @@
+const CACHE_NAME = "labirin-3d-v3";
+const ASSETS_TO_CACHE = [
+  "./",
+  "./index.html",
+  "./maze.js",
+  "./Three.js",
+  "./Box2dWeb.min.js",
+  "./jquery.js",
+  "./keyboard.js",
+  "./manifest.json",
+  // Gambar
+  "./assets/ball.png",
+  "./assets/concrete.png",
+  "./assets/brick.png",
+  "./assets/leftBtn.png",
+  "./assets/rightBtn.png",
+  "./assets/upBtn.png",
+  "./assets/downBtn.png",
+  "./assets/icon.png", // Icon baru
+  // Audio Baru
+  "./assets/bgsc.mp3",
+  "./assets/click.mp3",
+  "./assets/nxtlvl.mp3",
+];
+
+// Install Service Worker
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      console.log("Menyimpan aset ke cache...");
+      return cache.addAll(ASSETS_TO_CACHE);
+    })
+  );
+});
+
+// Activate & Hapus Cache Lama
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys().then((keyList) => {
+      return Promise.all(
+        keyList.map((key) => {
+          if (key !== CACHE_NAME) {
+            console.log("Menghapus cache lama", key);
+            return caches.delete(key);
+          }
+        })
+      );
+    })
+  );
+});
+
+// Fetch Aset
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+});
